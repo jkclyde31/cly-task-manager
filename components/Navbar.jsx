96ef7abed347ce7acc22eae9b498aac697ff7b43
jkClyde <aquin.randall@gmail.com>
2024-12-19 00:45:56 +0800

    added User image and name on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,15 @@
+'use client'
+
 import Image from "next/image"
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  const profileImage = session?.user?.image;
+
+
+
   return (
     <div className='flex items-center justify-between p-4'>
       {/* SEARCH BAR */}
@@ -9,20 +18,33 @@ const Navbar = () => {
         <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none"/>
       </div>
       {/* ICONS AND USER */}
-      {/* <div className='flex items-center gap-6 justify-end w-full'>
-        <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+      <div className='flex items-center gap-6 justify-end w-full'>
+        {/* <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
           <Image src="/message.png" alt="" width={20} height={20}/>
         </div>
         <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative'>
           <Image src="/announcement.png" alt="" width={20} height={20}/>
           <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>1</div>
         </div>
+        */}
+
         <div className='flex flex-col'>
-          <span className="text-xs leading-3 font-medium">John Doe</span>
-          <span className="text-[10px] text-gray-500 text-right">Admin</span>
-        </div>
-        <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/>
-      </div> */}
+          <span className="text-xs leading-3 font-medium">{session?.user?.name}</span>
+          {/* <span className="text-[10px] text-gray-500 text-right">Admin</span> */}
+        </div> 
+        <Image src={profileImage || Image} alt="" width={36} height={36} className="rounded-full"/>
+        <button
+                      onClick={() => {
+                        signOut({ callbackUrl: '/' });
+                      }}
+                      className='block px-4 py-2 text-sm text-gray-700'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-2'
+                    >
+                      Sign Out
+                    </button>
+      </div>
     </div>
   )
 }
