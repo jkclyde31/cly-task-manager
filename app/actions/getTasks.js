92f6f9517befb783f27a6d35f9df778862bc8d2d
jkClyde <aquin.randall@gmail.com>
2025-01-14 01:34:16 +0800

    reverse task

--- app/actions/getTasks.js
+++ app/actions/getTasks.js
@@ -10,8 +10,10 @@ import { authOptions } from "@/utils/AuthOptions";
 
 export async function getTasks() {
     await connectDB();
-    const tasks = await Task.find({}).populate('assignees', 'username image').lean();
-    return tasks;
+    const tasks = await Task.find({})
+    .populate('assignees', 'username image')
+    .sort({ requestDate: -1 })  
+    .lean();    return tasks;
 }
 
 
